2272ee13de4f1bd1d78686a78145f352adf95831
ViktoriaFekete <viktoria.fekete97@gmail.com>
2023-04-06 12:25:06 +0200

    Revert "Initial commit"
    This reverts commit eae76447c79b9c5f928565db950e88e48d3ff6a8.

--- scripts/content-script.js
+++
@@ -1,7 +0,0 @@
-(async () => {
-    const response = await chrome.runtime.sendMessage({greeting: "hello"});
-    // do something with response here, not outside the function
-    console.log(response);
-})();
-
-console.log("CS", document.querySelector('h1').textContent);
